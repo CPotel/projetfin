0d94687ee9e43e36db7f521663edc35b040459e5
YesiDudon <112876785+YesiDudon@users.noreply.github.com>
2023-05-15 14:53:31 +0200

    yo

---
+++ PHP & HTML/accueil.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <link href = "../CSS/Style_global.css" rel="stylesheet">
+        <link href = "home.css" rel="stylesheet">
+        <title>Pour l'instant c'est des trains</title>
+    </head>
+
+    <body>
+        <header>
+        </header>
+
+        <main>
+            <h1>Welcome to our game ! This is currently in heavy development, so there's nothing you can do right now.</h1>
+            <h2>But soon, you'll be able to work your brains out while figuring out train traffic.</h2>
+
+            <a href="game.php">You can access the game here (but it's empty, so there's no use going as of now)</a>
+
+
+        </main>
+        
+        <footer>
+            <div>Project by : Yesi DUDON, Esteban HALLIEZ, Anthony LEJEUNE, Sasha LE-ROUX ZIELINSKI, Robin MATARESE, Cyriaque POTEL (CIR 1)</div>
+        </footer>
+    </body>
+</html>
---
+++ PHP & HTML/game.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link href="game.css" rel="stylesheet">
+        <title>The game</title>
+    </head>
+    <body>
+        <h1>Welcome to Idraulico</h1>
+        <div class="game">Level n°</div>
+        <footer>
+            <div>Project by : Yesi DUDON, Esteban HALLIEZ, Anthony LEJEUNE, Sasha LE-ROUX ZIELINSKI, Robin MATARESE, Cyriaque POTEL (CIR 1)</div>
+        </footer>
+    </body>
+</html>
