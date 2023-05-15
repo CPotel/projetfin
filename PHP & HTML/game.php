9ad4afff856ae9f04c4bb7e2a2c0b3fd8cbe23f0
Sash-cmd <sasha.le-roux-zielinski@student.junia.com>
2023-05-15 15:11:45 +0200

    move des pages php

--- PHP & HTML/game.php
+++ PHP & HTML/game.php
@@ -3,11 +3,21 @@
     <head>
         <meta charset="utf-8">
         <link href="game.css" rel="stylesheet">
-        <title>The game</title>
+        <title>Game</title>
     </head>
     <body>
+        <h1>There's nothing to see here (as of now).</h1>
+        <main>
+            <?php
+                $output=NULL;
+                $output=exec('Jeu.exe');
+                echo $output;
+            ?>
+        </main>
+
         <h1>Welcome to Idraulico</h1>
         <div class="game">Level n°</div>
+
         <footer>
             <div>Project by : Yesi DUDON, Esteban HALLIEZ, Anthony LEJEUNE, Sasha LE-ROUX ZIELINSKI, Robin MATARESE, Cyriaque POTEL (CIR 1)</div>
         </footer>
