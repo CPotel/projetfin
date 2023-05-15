2d22162f8c594444068572690a29162694cbae9c
Anthonyjunia <anthony.lejeune@student.junia.com>
2023-05-15 14:29:06 +0200

    Fichier html fonctionnel

---
+++ Jeu/test.c
@@ -0,0 +1,8 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void main() {
+	int coucou = 15;
+	printf("%d", coucou);
+	return coucou; 
+}
--- game.php
+++ game.php
@@ -7,6 +7,14 @@
     </head>
     <body>
         <h1>There's nothing to see here (as of now).</h1>
+        <main>
+            <?php
+                $output=NULL;
+                $retval=NULL;
+                $output=exec('Jeu.exe');
+                echo $output;
+            ?>
+        </main>
         <footer>
             <div>Project by : Yesi DUDON, Esteban HALLIEZ, Anthony LEJEUNE, Sasha LE-ROUX ZIELINSKI, Robin MATARESE, Cyriaque POTEL (CIR 1)</div>
         </footer>
