8cc0da4103329ac5061187bec3e8954c1346f919
Sash-cmd <sasha.le-roux-zielinski@student.junia.com>
2023-05-16 14:58:59 +0200

    Login terminé (?) à voir

--- PHP & HTML/login_form.php
+++ PHP & HTML/login_form.php
@@ -13,19 +13,29 @@
 
 <body>
     <header>
+    <nav>
+            <ul>
+                <li><a class="accueil" href="accueil.php">HOME</a></li>
+                <li><a class="score" href="scores.php">SCORE</a></li>
+                <li><a class="theme" href="theme.php">THEME</a> </li>
+                <li><a class="login" href="login_form.html">MY ACCOUNT</a></li>
+                <li><a class="who" href="who.html">ABOUT US</a></li>
+            </ul>
+        </nav>
     </header>
     <main>
         <div class="boite">
-            <h1>Log in</h1>
-        <form method="post" action="login.php">
+            <h1 id="title">Log in</h1>
+        <form method="post" action="login.php" class="form">
                 <label>Name :</label>
-                <input type="text" name="name" required>
+                <input type="text" name="name"required>
                 <br><br>
                 <label>Password :</label>
                 <input type="password" name="passwd" required>
                 <br><br>
                 <input type="submit" name="submit" value="Log in" class="Bouton1">
         </form>
+        <p class="Signin">Don't have an account? <a href="form.php" class="link">Sign in</a></p>
 </div>
 </main>
 
