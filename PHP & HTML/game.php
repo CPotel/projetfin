445242710ae4fd198d6eb22d7a7aeb8465531173
Anthonyjunia <anthony.lejeune@student.junia.com>
2023-05-22 11:47:06 +0200

    game.php (header)

--- PHP & HTML/game.php
+++ PHP & HTML/game.php
@@ -5,18 +5,21 @@
         <link href="CSS/game.css" rel="stylesheet">
         <title>IDROLICO</title>
     </head>
-    <script href="header.js" defer></script>
     <body>
         <header>
-        <nav>
-            <ul>
-                <li><a class="accueil" href="index.php">HOME</a></li>
-                <li><a class="score" href="score.php">SCORE</a></li>
-                <li><a class="theme" href="theme.php">THEME</a> </li>
-                <li><a class="login" href="login_form.php">MY ACCOUNT</a></li>
-                <li><a class="who" href="who.html">ABOUT US</a></li>
-            </ul>
-        </nav>  
+            <a href="game.php"><img id="Titre" src="pictures/maynekraft_2.0.png" alt="Titre"></a>
+            
+            <div id="nav">
+            <nav>
+                <ul>
+                    <li><a class="accueil" href="accueil.php">HOME</a></li>
+                    <li><a class="score" href="score.php">SCORE</a></li>
+                    <li><a class="theme" href="theme.php">THEME</a> </li>
+                    <li><a class="login" href="login.php">MY ACCOUNT</a></li>
+                    <li><a class="who" href="who.html">ABOUT US</a></li>
+                </ul>
+            </nav>  
+            </div>
         </header>
         <main>
             <div class="game">
